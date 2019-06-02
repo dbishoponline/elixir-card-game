674a1af3c07cfdd68da35aba53666837d6f2d61b
Dylan Bishop <dbishoponline@gmail.com>
2019-06-02 16:51:44 -0700

    adds methods to shuffle, save, load deck to local file

--- lib/cards.ex
+++ lib/cards.ex
@@ -12,7 +12,40 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
+    suits = ["spades", "clubs", "hearts", "diamonds"]
+
+    cards = for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+
+    List.flatten(cards)
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file does not exist"
+    end
   end
 end
